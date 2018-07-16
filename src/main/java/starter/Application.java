e7f882e070a21be30c3633269485ca49a67adfef
caidixi <729195695@qq.com>
2018-07-16 14:49:01 +0800

    a new commit

--- src/main/java/starter/Application.java
+++ src/main/java/starter/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-    //start function
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
